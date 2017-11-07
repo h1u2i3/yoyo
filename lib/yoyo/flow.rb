a7d75cf66d0e26681032141d64d5f8f3a54c06b8
Xiaohui Gan <286390860@qq.com>
2017-11-07 16:59:22 +0800

    thought: add flow thought

--- lib/yoyo/flow.rb
+++ lib/yoyo/flow.rb
@@ -1,4 +1,17 @@
 module Yoyo
   class Flow
+    attr_reader :controller
+
+    def initialize(controller)
+      @controller = controller
+    end
+
+    def method_missing(name, *args, &block)
+      if controller.respond_to?(name)
+        controller.send(name, *args, &block)
+      else
+        super
+      end
+    end
   end
 end
---
+++ thought/flow_thought.rb
@@ -0,0 +1,11 @@
+class LoginFlow < Yoyo::Flow
+  # crud
+  crud  :session
+  c     :session
+  ru    :session 
+
+
+  # should pass a flow object to the view?
+  # use to directly pass the controller to the view?
+  # how to implement this protocol?
+end
